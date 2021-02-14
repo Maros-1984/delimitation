5496ebc8a4290e2ee0bd692c15f2651ff2b55ee0
Vranec, Maros (MONETA, consultant) <maros.vranec@moneta.cz>
2021-02-14 18:11:29 +0100

    Simple game grid display in order to determine ideal backend payload.

--- delimitation-frontend/src/App.js
+++ delimitation-frontend/src/App.js
@@ -1,33 +1,109 @@
-import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from "react";
+import {Fragment, useEffect} from "react";
 import {backendUrl} from "./configuration";
+import {AreaColor} from "./constants";
 
 const App = () => {
-    const [backendResponse, setBackendResponse] = useState()
-
     useEffect(() => {
         fetch(backendUrl)
             .then(response => response.json())
-            .then(data => setBackendResponse(data));
+            .then(data => console.log(data));
     }, [])
 
+    const gameState = {
+        areas: [
+            [AreaColor.EMPTY, AreaColor.RED, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY],
+            [AreaColor.EMPTY, AreaColor.BLUE, AreaColor.BLUE, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY],
+            [AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY],
+            [AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY],
+            [AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY],
+            [AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY],
+            [AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY],
+            [AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY],
+            [AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY],
+            [AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY, AreaColor.EMPTY],
+        ],
+        possibleMoves: [
+            {
+                areaX: 0,
+                areaY: 0,
+                bottom: true
+            },
+            {
+                areaX: 0,
+                areaY: 0,
+                right: true
+            },
+        ]
+
+    }
+
+    const areaWidth = 90 / (gameState.areas[0].length) + '%';
+    const borderWidth = 10 / (gameState.areas[0].length + 1) + '%';
+    const areaStyle = {
+        paddingBottom: areaWidth,
+        width: areaWidth,
+    };
+    const verticalBorderStyle = {
+        paddingBottom: areaWidth,
+        width: borderWidth,
+    }
+    const horizontalBorderStyle = {
+        paddingBottom: borderWidth,
+        width: areaWidth,
+    }
+    const dotStyle = {
+        paddingBottom: borderWidth,
+        width: borderWidth,
+    }
+
+    const isPossibleRightMove = (rowIndex, cellIndex) => gameState.possibleMoves.find(pm => pm.areaX === cellIndex && pm.areaY === rowIndex && pm.right);
+    const isPossibleBottomMove = (rowIndex, cellIndex) => gameState.possibleMoves.find(pm => pm.areaX === cellIndex && pm.areaY === rowIndex && pm.bottom);
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload. {backendResponse?.hello}
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div>
+            &nbsp;
+            <table>
+                <tbody>
+                {gameState.areas.map((row, rowIndex) => {
+                    return (
+                        <Fragment>
+                            <tr key={'row-' + rowIndex}>
+                                {row.map((cell, cellIndex) => {
+                                    return (<Fragment>
+                                            <td key={'cell-' + rowIndex + "-" + cellIndex} style={{
+                                                ...areaStyle,
+                                                backgroundColor: cell === AreaColor.RED ? 'red' : cell === AreaColor.BLUE ? 'blue' : 'white'
+                                            }}/>
+                                            {cellIndex !== gameState.areas[0].length - 1 &&
+                                            <td key={'right-border-of-cell-' + rowIndex + "-" + cellIndex}
+                                                style={verticalBorderStyle}
+                                                className={isPossibleRightMove(rowIndex, cellIndex) ? 'possibleMove border' : 'border'}/>}
+                                        </Fragment>
+                                    )
+                                })
+                                }
+                            </tr>
+                            {rowIndex !== gameState.areas.length - 1 && (
+                                <tr key={'bottom-borders-of-row-' + rowIndex}>
+                                    {row.map((cell, cellIndex) => {
+                                        return (<Fragment>
+                                                <td key={'bottom-border-of-cell-' + rowIndex + "-" + cellIndex}
+                                                    style={horizontalBorderStyle}
+                                                    className={isPossibleBottomMove(rowIndex, cellIndex) ? 'possibleMove border' : 'border'}
+                                                />
+                                                {cellIndex !== gameState.areas[0].length - 1 &&
+                                                <td key={'dot-between-borders-' + rowIndex + "-" + cellIndex}
+                                                    style={dotStyle} className={'border'}/>}
+                                            </Fragment>
+                                        )
+                                    })
+                                    }
+                                </tr>)}
+                        </Fragment>)
+                })}
+                </tbody>
+            </table>
         </div>
     );
 };
---
+++ delimitation-frontend/src/constants.js
@@ -0,0 +1,6 @@
+export const AreaColor =
+    {
+        EMPTY: 'EMPTY',
+        RED: 'RED',
+        BLUE: 'BLUE'
+    }
