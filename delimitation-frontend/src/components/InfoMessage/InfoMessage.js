7677c64a15ab7188b3d71eaafa999bbbebad1b74
Vranec, Maros (MONETA, consultant) <maros.vranec@moneta.cz>
2021-02-17 22:58:36 +0100

    Small frontend tweaks.

--- delimitation-frontend/src/components/InfoMessage/InfoMessage.js
+++ delimitation-frontend/src/components/InfoMessage/InfoMessage.js
@@ -6,7 +6,7 @@ export const InfoMessage = ({game}) => {
     if (game.possibleMoves.length === 0) {
         return (
             <div className="infoMessage">
-                <h1 className="glow">Wait for <span
+                <h1 className="glow">Wait for the <span
                     style={{color: game.playerOnMove.toLowerCase()}}>other player</span> to move.</h1>
                 {game.moves.length <= 1 && <p>Send him the URL if you have not already.</p>}
             </div>);
@@ -24,13 +24,16 @@ export const InfoMessage = ({game}) => {
             <div className="infoMessage">
                 <h1>Game Over</h1>
                 {blue > red && (
-                    <h2><span style={{color: 'blue'}}>BLUE</span> player won {blue} tiles to <span
-                        style={{color: 'red'}}>RED</span>'s {red} tiles.</h2>)}
+                    <h2><span style={{color: 'blue'}}>BLUE</span> player won <span
+                        style={{color: 'blue'}}>{blue}</span> tiles to <span
+                        style={{color: 'red'}}>RED</span>'s <span style={{color: 'red'}}>{red}</span> tiles.</h2>)}
                 {blue < red && (
-                    <h2><span style={{color: 'red'}}>RED</span> player won {red} tiles to <span
-                        style={{color: 'blue'}}>BLUE</span>'s {blue} tiles.</h2>)}
+                    <h2><span style={{color: 'red'}}>RED</span> player won <span
+                        style={{color: 'red'}}>{red}</span> tiles to <span
+                        style={{color: 'blue'}}>BLUE</span>'s <span style={{color: 'blue'}}>{blue}</span> tiles.</h2>)}
                 {blue === red && (
-                    <h2>It's a draw. <span style={{color: 'blue'}}>BLUE</span> player has {blue} tiles as well as <span
+                    <h2>It's a draw. <span style={{color: 'blue'}}>BLUE</span> player has <span
+                        style={{color: 'blueviolet'}}>{blue}</span> tiles as well as <span
                         style={{color: 'red'}}>RED</span> player.</h2>)}
             </div>);
     }
